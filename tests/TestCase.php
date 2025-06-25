42102f82fc3bc70cecb31199a9d3dfa3a87a2931
Anton Zabolotnikob <azabolotnikov@ronasit.com>
2025-06-25 15:06:36 +0500

    feat: add tests with coverage

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace RonasIT\Clerk\Tests;
 
-use RonasIT\Clerk\Providers\ClerkServiceProvider;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
@@ -10,17 +9,5 @@ class TestCase extends BaseTestCase
     public function setUp(): void
     {
         parent::setUp();
-    }
-
-    protected function defineEnvironment($app): void
-    {
-        $app->setBasePath(__DIR__ . '/..');
-    }
-
-    protected function getPackageProviders(): array
-    {
-        return [
-            ClerkServiceProvider::class,
-        ];
     }
 }
